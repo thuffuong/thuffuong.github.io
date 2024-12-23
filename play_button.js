6021df1205addc0bb04160be7f8548a89f0deda9
thuffuong <nhatzittt@gmail.com>
2024-12-23 12:06:16 +0700

    Add files via upload

---
+++ play_button.js
@@ -0,0 +1,23 @@
+class PlayButton extends HTMLElement {
+    static register(tagName) {
+      if ("customElements" in window) {
+        customElements.define(tagName || "play-button", PlayButton);
+      }
+    }
+  
+    connectedCallback() {
+      this.button.addEventListener("click", (event) => {
+        !this.player.paused ? this.player.pause() : this.player.play();
+      });
+    }
+  
+    get player() {
+      return this.querySelector("audio, video");
+    }
+  
+    get button() {
+      return this.querySelector("button");
+    }
+  }
+  
+  PlayButton.register();
